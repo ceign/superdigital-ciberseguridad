ecaa6d650abe57c64ee8d7202704d97410eb22d0
ceign <dnssdvlp@gmail.com>
2021-09-06 20:13:52 -0300

    Build V.1 not minified.

---
+++ app.js
@@ -0,0 +1,79 @@
+// TRANSITIONS
+
+ScrollReveal().reveal(".intro__image img", { delay: 200 });
+
+ScrollReveal().reveal(".intro__title h1 div", { delay: 200 });
+ScrollReveal().reveal(".intro__title h1 span:nth-child(2)", { delay: 600 });
+ScrollReveal().reveal(".intro__title h1 span:last-child", { delay: 1000 });
+
+ScrollReveal().reveal(".step-1__text .step__paragraph", {
+  delay: 1200,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-2__text .step__paragraph", {
+  delay: 500,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "bottom",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-3__text .step__paragraph", {
+  delay: 500,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-4__text .step__paragraph", {
+  delay: 900,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "right",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-5__text .step__paragraph", {
+  delay: 500,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "bottom",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-6__text .step__paragraph", {
+  delay: 500,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "left",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-7__text .step__paragraph", {
+  delay: 800,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "right",
+  distance: "60px",
+});
+
+ScrollReveal().reveal(".step-8__text .step__paragraph", {
+  delay: 500,
+  duration: 600,
+  reset: false,
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  origin: "bottom",
+  distance: "60px",
+});
